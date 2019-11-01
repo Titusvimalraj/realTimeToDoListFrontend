ef283fde37ec410db4cc37f6d861088ba74bea95
Titusvimalraj <titusvimalraj@gmail.com>
2019-11-01 23:28:12 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { hammerjs } from 'hammerjs';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { LoginComponent } from './user/login/login.component';
+import { GlobalConfig } from './globalConfig';
+import { SharedModule } from './shared/shared.module';
+import { TodoListModule } from './todo-list/todo-list.module';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    UserModule,
+    SharedModule,
+    TodoListModule,
+    HttpClientModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      closeButton: true
+    }),
+    RouterModule.forRoot([
+      { path: `${GlobalConfig.apiVersion}/users/login`, component: LoginComponent, pathMatch: 'full' },
+      { path: '', redirectTo: `${GlobalConfig.apiVersion}/users/login`, pathMatch: 'full' },
+      { path: '*', component: LoginComponent, pathMatch: 'full' },
+      { path: '**', component: LoginComponent, pathMatch: 'full' }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/globalConfig.ts
@@ -0,0 +1,6 @@
+//to keep track of versions in case if server url is changed
+export class GlobalConfig {
+    public static serverUrl =  "http://api.realtimetodolist.info";
+    public static clientUrl = "http://realtimetodolist.info";
+    public static apiVersion = "api/v1";
+}
